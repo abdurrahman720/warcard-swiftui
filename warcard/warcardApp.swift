19d1cbb96545e0e288e317b0af1cf436162b6b2c
Abdur Rahman <personal.abdurrahman@gmail.com>
2024-05-08 16:06:29 +0600

    Initial Commit

---
+++ warcard/warcardApp.swift
@@ -0,0 +1,17 @@
+//
+//  warcardApp.swift
+//  warcard
+//
+//  Created by Abdur Rahman on 8/5/24.
+//
+
+import SwiftUI
+
+@main
+struct warcardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
